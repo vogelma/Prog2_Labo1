82535ce8220e426e1889639340fbaa8877b9a5b1
Maelle <maelle.vogel@heig-vd.ch>
2021-04-22 09:04:15 +0200

    create git

---
+++ mainTest.c
@@ -0,0 +1,19 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : mainTest.h
+ Auteur(s)      :
+ Date creation  : 22.04.2021
+
+ Description    :
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ -----------------------------------------------------------------------------------
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
